651b2652be07b58647cf4647ef3466e2471057e6
Dov Kruger <Dov.Kruger@gmail.com>
2019-01-23 09:35:32 -0500

    consolidated hw0 into one directory

---
+++ hw0-C++review/reviewbasics/computesums.cc
@@ -0,0 +1,22 @@
+// sum the numbers from 1 to n and return the answer
+int sum(int n) {
+
+
+}
+
+// sum the numbers from 1/1 to 1/n and return
+float sumReciprocals(int n) {
+
+	
+}
+
+// sum the numbers from 1/n to 1/1 (same as above but reversed) and return
+float sumReciprocalsReverse(int n) {
+
+}
+
+int main() {
+	cout << sum(100) << '\n';
+	cout << sumReciprocals(100) << '\n';
+	cout << sumReciprocalsReverse(100) << '\n';
+}
