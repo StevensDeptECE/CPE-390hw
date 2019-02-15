dc5f11a4300945ab04b0ad2f196c083180cf7cb3
Dov Kruger <Dov.Kruger@gmail.com>
2019-02-15 11:37:57 -0500

    simplified hw3

--- hw3bignumber_addition/BigNum.hh
+++ hw3bignumber_addition/BigNum.hh
@@ -6,12 +6,11 @@ class BigNum {
 private:
 	// 1234
 	uint32_t a, b, c, d; // a is the high order part
-	BigNum() {}
 public:
+	BigNum() {}
 	BigNum(uint32_t v) : a(0), b(0), c(0), d(v) {}
 	
-	friend BigNum add128(const BigNum& left, const BigNum& right);
-	friend BigNum operator +(const BigNum& left, const BigNum& right);
+	friend BigNum add128(BigNum& dest, const BigNum& left, const BigNum& right); // dest = left + right
 
 	friend std::ostream& operator <<(std::ostream& s, const BigNum& big) {
 		return s << big.a << "," << big.b << "," << big.c << "," << big.d;
