b6c644ace8e08f67ecad67e16fa5c0aab378f0bf
Dov Kruger <dov.kruger@stevens.edu>
2019-11-01 08:48:26 -0400

    added explanation of DovCrypt method

--- hw11-asm-dovcrypt/main.cc
+++ hw11-asm-dovcrypt/main.cc
@@ -1,6 +1,28 @@
 #include <iostream>
 using namespace std;
 
+/*
+  The Dovcrypt algorithm is simple but not very secure.
+  To encrypt or decrypt, xor each byte of the secret message with a byte of the key.
+  Each time, advance the index into the key to use the next byte. If you reach the end of the key
+  start at the beginning again.
+
+  For example, to encrypt the message:
+
+  hello there
+
+  with the key: Dov
+
+  xor the message below with the letter shown directly below:
+
+  hello there
+  DovDovDovDo
+
+  The result will be fairly unreadable to the casual observer.
+
+  To get the original message back, xor the encrypted message in the same way.
+ */
+
 void crypt(char* dest, const char* src, uint32_t len, const char* key);
 
 void print(const char msg[], uint32_t len) {
