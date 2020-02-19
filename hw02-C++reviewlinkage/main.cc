a6cac71ca08b4e20644d4554e70e9ddbe324fca2
Dov Kruger <dov.kruger@stevens.edu>
2020-02-19 13:12:03 -0500

    fixed hw02 review linkage

--- hw02-C++reviewlinkage/main.cc
+++ hw02-C++reviewlinkage/main.cc
@@ -1,13 +1,11 @@
-#include "Fraction.hh"
-
 // compute the hypotenuse given the two sides
-double hypot(double a, double b);
+double hypotenuse(double a, double b);
 
 // compute the average of the two values
 double mean(double a, double b);
 
 int main() {
-  cout << hypot(3, 4) << '\n';
-  cout << hypot(4.0, 5.0) << '\n';
+  cout << hypotenuse(3, 4) << '\n';
+  cout << hypotenuse(4.0, 5.0) << '\n';
   cout << mean(1.0, 2.0) << '\n';
 }
